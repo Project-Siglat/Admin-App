420b358e52b4a8b3c58b48bd37af0b47f521451b
renzaspiras <aspirasrenz@gmail.com>
2025-08-30 23:57:47 +0800

    changes the port to 2424

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    port: 2424, // change this to the port you want
+    strictPort: true, // (optional) if true, Vite will fail instead of trying next available port
+    host: "0.0.0.0"  // (optional) listen on all network interfaces
+  }
 });
